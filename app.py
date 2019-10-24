1004bc3f5e3eea579d27d6512a7569677b562ca3
dwb217 <davidwbender@gmail.com>
2019-10-24 19:57:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ tabtitle = 'Dash Template'
 sourceurl = 'https://plot.ly/python/choropleth-maps/'
 githublink = 'https://github.com/austinlasseter/dash-template'
 
+### read in Data
+df = pd.read_pickle('virginia_total.pkl')
+print(df['jurisdiction'].value_counts().sort_index().index)
+
+
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -21,12 +26,50 @@ app.title=tabtitle
 
 app.layout = html.Div(children=[
     html.H1('This is the title'),
-    html.Div(children=['Stuff will go here']),
+    html.Div(children=[
+        dcc.Dropdown(
+            id-'my-dropdown',
+            options=[{'label':i, 'value':i} for i in options_list}],
+            value='FAIRFAX CITY',
+        ),
+    html.Br(),
+    dcc.Graph(id='output_div', children=''),
+    html.Br(),
+
+    ]),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
     ]
 )
+
+@app.callback(Output('output-div', 'figure'),
+                [Input('my-dropdown', 'value')])
+def county_picker(arg1):
+    juris_df=df[df['jurisdiction']==arg1]
+            mydata1 = go.Bar(x=list(accomack['precinct'].value_counts().index),
+                     y=list(accomack['votes']['Donald Trump']),
+                     marker=dict(color='#122A7F'),
+                     name='Trump')
+            mydata2 = go.Bar(x=list(accomack['precinct'].value_counts().index),
+                     y=list(accomack['votes']['Hillary Clinton']),
+                     marker=dict(color='#008080'),
+                     name='Clinton')
+            mydata3 = go.Bar(x=list(accomack['precinct'].value_counts().index),
+                     y=list(accomack['votes']['Other']),
+                     marker=dict(color='#92A5E8'),
+                     name='Other')
+
+            mylayout = go.Layout(
+                    title='Grouped bar chart',
+                    xaxis=dict(title='Candidates'),
+                    yaxis=dict(title='Number of Votes')    
+                    )
+fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
+return fig
+
+
+
 
 ############ Callbacks
 
